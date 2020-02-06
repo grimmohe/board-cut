7bf515e87f9b4320c392d9b4d91c31aa6f906bb5
christoph <chris.o.henning@gmail.com>
2020-02-06 20:14:52 +0100

    fix overlapping

--- src/app/cut/used-area-calculation.ts
+++ src/app/cut/used-area-calculation.ts
@@ -3,15 +3,18 @@ import { Direction, UsedPart, UsedStock } from 'app/app.model';
 export class UsedAreaCalculation {
   static updateUsedArea(usedStock: UsedStock, newUsedParts: UsedPart[], direction: Direction) {
     newUsedParts.forEach((usedPart) => {
+      const partWidth = usedPart.turned ? usedPart.part.height : usedPart.part.width;
+      const partHeight = usedPart.turned ? usedPart.part.width : usedPart.part.height;
+
       if (direction === 'x') {
         usedStock.usedArea.y = Math.max(
           usedStock.usedArea.y,
-          usedPart.position.y + usedPart.part.height + usedStock.stock.material.cuttingWidth
+          usedPart.position.y + partHeight + usedStock.stock.material.cuttingWidth
         );
       } else {
         usedStock.usedArea.x = Math.max(
           usedStock.usedArea.x,
-          usedPart.position.x + usedPart.part.width + usedStock.stock.material.cuttingWidth
+          usedPart.position.x + partWidth + usedStock.stock.material.cuttingWidth
         );
       }
     });
